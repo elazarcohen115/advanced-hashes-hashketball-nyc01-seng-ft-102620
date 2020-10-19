811f062ea6e8f314476bcf58d8df59c987379dbb
simon cohen <escohen115@gmail.com>
2020-10-19 22:19:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -190,14 +190,15 @@ end
 
 def big_shoe_rebounds
 shoe_size = game_hash[:home][:players][0][:shoe]
+
   game_hash.each do |k, v|
     v[:players].each do |hash|
       if hash[:shoe] > shoe_size
         shoe_size = hash[:shoe]
       end
     end
-    binding.pry
   end
+
   game_hash.each do |k, v|
     v[:players].each do |hash|
       if hash.values_at(:shoe) == shoe_size
