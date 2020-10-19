a3fc15d86a5b88c7991b0c4dcfcdc472678f6c0e
simon cohen <escohen115@gmail.com>
2020-10-19 20:48:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,7 +131,7 @@ end
 def num_points_scored (player)
   game_hash.each do |k,v|
     v.each do |ik, iv|
-      iv.each do |iik, iiv|
+      ik.each do |iik, iiv|
         binding.pry
       end
     end
