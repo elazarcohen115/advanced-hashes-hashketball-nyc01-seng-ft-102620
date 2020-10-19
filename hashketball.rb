19b0b8935e19c6bec07ff9145ca364507048781e
simon cohen <escohen115@gmail.com>
2020-10-19 22:12:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -193,8 +193,9 @@ shoe_size = game_hash[:home][:players][0][:shoe]
   game_hash.each do |k, v|
     v[:players].each do |hash|
       if hash[:shoe] > shoe_size
-        binding.pry
+
         shoe_size == :hash[:shoe]
+                binding.pry
       end
     end
   end
